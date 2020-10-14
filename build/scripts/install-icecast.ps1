786e7511f6eacbed280d9c0023b07093f9b85b05
Dave Eargle <dave@daveeargle.com>
2020-10-13 22:40:02 -0600

    wrap in if-else instead of exit

--- build/scripts/install-icecast.ps1
+++ build/scripts/install-icecast.ps1
@@ -1,11 +1,11 @@
 Set-PSDebug -Trace 1
 if (Test-Path "C:\Program Files (x86)\Icecast2 Win32\Icecast2.exe" -PathType Leaf){
-  Write-Host "Icecast looks to be already installed. Exiting."
-  Exit
+  Write-Host "Icecast looks to be already installed. Skipping..."
+} else {
+  cd "C:\Users\vagrant\Downloads"
+  wget 'https://ftp.osuosl.org/pub/xiph/releases/icecast/icecast2_win32_2.0.1_setup.exe' -out 'icecast_setup.exe'
+  "icecast_setup.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-" | cmd
 }
-cd "C:\Users\vagrant\Downloads"
-wget 'https://ftp.osuosl.org/pub/xiph/releases/icecast/icecast2_win32_2.0.1_setup.exe' -out 'icecast_setup.exe'
-"icecast_setup.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-" | cmd
 
 # Create desktop shortcut
 $WshShell = New-Object -comObject WScript.Shell
