a12697420d4887dab67050ba287c732ba9fd72c4
Dave Eargle <dave@daveeargle.com>
2021-08-23 23:57:27 -0600

    switch to public directory instead

--- build/scripts/install-icecast.ps1
+++ build/scripts/install-icecast.ps1
@@ -7,18 +7,9 @@ if (Test-Path "C:\Program Files (x86)\Icecast2 Win32\Icecast2.exe" -PathType Lea
   "icecast_setup.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-" | cmd
 }
 
-$USERNAME = "Labuser"
-
-# Create desktop directory if not exists
-$path = "C:\Users\$USERNAME\Desktop"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
-
 # Create desktop shortcut
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$path\Icecast2 Win32.lnk")
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Icecast2 Win32.lnk")
 $Shortcut.TargetPath = "C:\Program Files (x86)\Icecast2 Win32\Icecast2.exe"
 $Shortcut.WorkingDirectory = "C:\Program Files (x86)\Icecast2 Win32"
 $Shortcut.save()
